eb3bed6d1fdb1d072cba006f36c91d3b64649c3e
Maximilian Starke <maximilian.starke@medionmail.com>
2023-05-29 10:03:28 +0200

    fixes forwarding compound, ...

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ class svg_generator {
 		(void)nested_content;
 		return std::string();
 	}
-		//< svg width = "6400" height = "1600" version = "1.1" xmlns = "http://www.w3.org/2000/svg" >
+	//< svg width = "6400" height = "1600" version = "1.1" xmlns = "http://www.w3.org/2000/svg" >
 
 
 
@@ -59,13 +59,13 @@ class svg_environment : public svg_generator {
 + "</svg>";
 	}
 
-	std::unique_ptr<svg_environment> body;
+	std::unique_ptr<svg_generator> body;
 	std::string height;
 	std::string width;
 
 public:
 
-	svg_environment(const std::string& height, const std::string& width, std::unique_ptr<svg_environment> body) :
+	svg_environment(const std::string& height, const std::string& width, std::unique_ptr<svg_generator> body) :
 		body(std::move(body)),
 		height(height),
 		width(width)
@@ -85,13 +85,13 @@ public:
 
 	std::vector<u_ptr> elements;
 
-	svg_compound(){}
+	svg_compound() {}
 
 	//svg_compound(std::initializer_list<std::unique_ptr<svg_compound>> init) : elements(init) {}
 
 	template<class... T>
-	svg_compound(T... init) : {
-		elements.push_back(std::move(init));...
+	svg_compound(T&&... init) {
+		((void)elements.push_back(std::forward<T>(init)), ...);
 	}
 
 	virtual std::string get_svg() const override {
@@ -106,23 +106,25 @@ public:
 };
 
 void test() {
-	
-	auto grid	= std::make_unique<svg_compound>();
-	auto walls	= std::make_unique<svg_compound>();
+
+	auto grid = std::make_unique<svg_compound>();
+	auto walls = std::make_unique<svg_compound>();
 	auto robots = std::make_unique<svg_compound>();
 
-	/*
 	auto body = std::make_unique<svg_compound>(
-		 grid, walls, robots 
-	);
-	
-	auto svg_env = std::make_unique<svg_environment>(body);
+		std::move(grid),
+		std::move(walls),
+		std::move(robots)
+		);
+
+	auto svg_env = std::make_unique<svg_environment>("400", "400", std::move(body));
 
 	svg_compound svg_file;
 	svg_file.elements.emplace_back(std::make_unique<xml_header>());
 	svg_file.elements.emplace_back(std::move(svg_env));
 
 	standard_logger()->info(svg_file.get_svg());
+	/*
 	*/
 
 }
