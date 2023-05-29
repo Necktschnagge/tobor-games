d43cceefdd2db94bd8545a5c7f4838a344d46769
Maximilian Starke <maximilian.starke@medionmail.com>
2023-05-29 09:31:26 +0200

    some fixes

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "engine.h"
 
 #include <string>
+#include <numeric>
 
 class svg_generator {
 
@@ -10,8 +11,13 @@ class svg_generator {
 )xxx";
 	}
 
-	static std::string wrap_svg(const std::string& height, const std::string& width, const std::string& nested_content)
-		< svg width = "6400" height = "1600" version = "1.1" xmlns = "http://www.w3.org/2000/svg" >
+	static std::string wrap_svg(const std::string& height, const std::string& width, const std::string& nested_content) {
+		(void)height;
+		(void)width;
+		(void)nested_content;
+		return std::string();
+	}
+		//< svg width = "6400" height = "1600" version = "1.1" xmlns = "http://www.w3.org/2000/svg" >
 
 
 
@@ -22,19 +28,21 @@ public:
 		return svg_result;
 	}
 
-	std::string get_svg() const virtual = 0;
 
+	virtual std::string get_svg() const = 0;
+
+	virtual ~svg_generator() {
+	}
 
 };
 
 class xml_header : public svg_generator {
 
 public:
-	std::string get_svg() const virtual override {
+	virtual std::string get_svg() const override {
 		return R"xxx(<?xml version="1.0" standalone="no"?>
 )xxx";
 	}
-
 };
 
 
@@ -51,32 +59,75 @@ class svg_environment : public svg_generator {
 + "</svg>";
 	}
 
-	const svg_generator& body;
+	std::unique_ptr<svg_environment> body;
 	std::string height;
 	std::string width;
 
 public:
 
-	svg_environment(const std::string& height, const std::string& width, const svg_generator& body) :
-		body(body),
+	svg_environment(const std::string& height, const std::string& width, std::unique_ptr<svg_environment> body) :
+		body(std::move(body)),
 		height(height),
 		width(width)
 	{}
 
-	std::string get_svg() const virtual override {
-		return wrap_svg(height, width, body.get_svg());
+	virtual std::string get_svg() const override {
+		return wrap_svg(height, width, body->get_svg());
 	}
 
 };
 
+class svg_compound : public svg_generator {
+
+public:
+
+	using u_ptr = std::unique_ptr<svg_generator>;
+
+	std::vector<u_ptr> elements;
+
+	svg_compound(){}
+
+	//svg_compound(std::initializer_list<std::unique_ptr<svg_compound>> init) : elements(init) {}
+
+	template<class... T>
+	svg_compound(T... init) : {
+		elements.push_back(std::move(init));...
+	}
+
+	virtual std::string get_svg() const override {
+		return std::accumulate(
+			elements.cbegin(),
+			elements.cend(),
+			std::string(),
+			[](const std::string& acc, const u_ptr& el) {
+				return acc + el->get_svg();
+			});
+	}
+};
 
 void test() {
+	
+	auto grid	= std::make_unique<svg_compound>();
+	auto walls	= std::make_unique<svg_compound>();
+	auto robots = std::make_unique<svg_compound>();
 
+	/*
+	auto body = std::make_unique<svg_compound>(
+		 grid, walls, robots 
+	);
+	
+	auto svg_env = std::make_unique<svg_environment>(body);
 
-	body = grid + walls + robots;
-	file = header + svg_environment(body);
+	svg_compound svg_file;
+	svg_file.elements.emplace_back(std::make_unique<xml_header>());
+	svg_file.elements.emplace_back(std::move(svg_env));
+
+	standard_logger()->info(svg_file.get_svg());
+	*/
 
 }
+#if false
+#endif
 
 
 int main()
