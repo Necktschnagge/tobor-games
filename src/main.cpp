820e80ad0589771d3dbb65ed1a6b9ecf630587f3
Maximilian Starke <maximilian.starke@medionmail.com>
2023-06-15 15:43:44 +0200

    some svg stuff

--- src/main.cpp
+++ src/main.cpp
@@ -105,9 +105,115 @@ public:
 	}
 };
 
+class svg_primitive : public svg_generator {
+
+public:
+
+	using u_ptr = std::unique_ptr<svg_generator>;
+
+	//std::vector<u_ptr> elements;
+	std::string primitive;
+
+	svg_primitive() {}
+
+	//svg_compound(std::initializer_list<std::unique_ptr<svg_compound>> init) : elements(init) {}
+
+	//template<class... T>
+	svg_primitive(const std::string& primitive) : primitive(primitive) {}
+
+	virtual std::string get_svg() const override {
+		return primitive;
+	}
+};
+
+
+class svg_path : public svg_generator {
+
+public:
+
+	class element {
+
+	public:
+
+		virtual std::string str() const = 0;
+
+
+
+	};
+
+	template <class Number>
+	class M : public element {
+
+		std::vector<std::pair<Number, Number>> coordinates;
+
+		virtual std::string str() const override {
+			return std::accumulate(coordinates.cbegin(), coordinates.cend(), "M",
+				[](const std::string& acc, const std::pair<Number, Number>& el) -> std::string {
+					return acc + " " + std::to_string(el.first) + " " + std::to_string(el.second);
+				}
+			);
+		}
+	};
+
+	using u_ptr = std::unique_ptr<svg_generator>;
+
+	//std::vector<u_ptr> elements;
+	//std::string primitive;
+	using map = std::map<std::string, std::string>;
+	map other_properties;
+	std::vector<std::shared_ptr<element>> path_elements;
+
+
+	svg_path() {}
+
+	//svg_compound(std::initializer_list<std::unique_ptr<svg_compound>> init) : elements(init) {}
+
+	//template<class... T>
+	//svg_path(const std::string& primitive) : primitive(primitive) {}
+
+	virtual std::string get_svg() const override {
+		auto result = std::string("<path");
+
+		result += std::string(" d=\"") +
+			std::accumulate(
+				path_elements.cbegin(),
+				path_elements.cend(),
+				std::string(""),
+				[](const std::string& acc, const std::shared_ptr<element>& el) -> std::string {
+					return acc + el->str();
+				})
+			+ "\"";
+				result += std::accumulate(
+					other_properties.cbegin(),
+					other_properties.cend(),
+					std::string(""),
+					[](std::string& acc, const map::value_type& el) -> std::string {
+						return acc + " " + el.first + "=" + "\"" + el.second + "\"";
+					});
+				result += std::string("/>");
+				return result;
+	}
+};
+
 void test() {
 
-	auto grid = std::make_unique<svg_compound>();
+
+
+
+	const std::string test_s{ R"xxx(<path d="M 10 70 l 200 100 l 200 50 z"
+        fill="red" stroke="blue" stroke-width="3" />
+)xxx"};
+	const std::string test_s2{ R"xxx(<path d="M 0 0 h 400 v 400 h -400 v -400 z"
+        fill="grey" stroke="black" stroke-width="10" />
+)xxx"};
+
+	auto example_path_element = std::make_unique<svg_primitive>(test_s);
+	auto outside_box = std::make_unique<svg_primitive>(test_s2);
+
+	//<rect width="100%" height="100%" fill="grey" />
+
+
+	auto grid = std::make_unique<svg_compound>(std::move(example_path_element), std::move(outside_box));
 	auto walls = std::make_unique<svg_compound>();
 	auto robots = std::make_unique<svg_compound>();
 
@@ -115,7 +221,7 @@ void test() {
 		std::move(grid),
 		std::move(walls),
 		std::move(robots)
-		);
+	);
 
 	auto svg_env = std::make_unique<svg_environment>("400", "400", std::move(body));
 
@@ -135,6 +241,7 @@ void test() {
 int main()
 {
 	init_logger();
+	test();
 	return 0;
 }
 
