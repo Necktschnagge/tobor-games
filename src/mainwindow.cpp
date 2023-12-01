f4b69e842447495032af525f5efd1df0b2afeede
Maximilian Starke <maximilian.starke@medionmail.com>
2023-12-02 00:49:23 +0100

    first part of fixing unmanaged pointers

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -3,24 +3,26 @@
 #include "./ui_mainwindow.h"
 #include <QMessageBox>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget* parent)
+	: QMainWindow(parent)
+	, ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+	ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+	delete ui;
 }
 
 
 
 void MainWindow::on_actionshowSVG_triggered()
 {
-    const QString example_svg_string {
-      R"xxx(<?xml version="1.0" ?>
+	for (int i = 0; i < 300; ++i) {
+
+		const QString example_svg_string{
+		  R"xxx(<?xml version="1.0" ?>
 <!DOCTYPE svg  PUBLIC '-//W3C//DTD SVG 1.1//EN'  'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'>
 <svg enable-background="new 0 0 512 512.068" height="512.068px" id="Layer_1" version="1.1" viewBox="0 0 512 512.068" width="512px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
 <g id="meanicons_x5F_23">
@@ -29,30 +31,59 @@ void MainWindow::on_actionshowSVG_triggered()
 <g id="Layer_1_1_"/>
 </svg>
 )xxx"
-    };
+		};
 
-    QXmlStreamReader xml;
-    xml.addData(example_svg_string);
+		QXmlStreamReader xml;
+		xml.addData(example_svg_string);
 
-    QSvgRenderer* svgRenderer = new QSvgRenderer(&xml);
-    QGraphicsSvgItem *item = new QGraphicsSvgItem();
-    QGraphicsScene *scene = new QGraphicsScene();
+		// https://stackoverflow.com/questions/36026593/how-to-add-children-to-qgraphicsitem
+		//auto graphicsView = new QGraphicsView();
 
-    item->setSharedRenderer(svgRenderer);
-    scene->addItem(item);
-    ui->graphicsView->setScene(scene);
-    ui->graphicsView->fitInView(scene->sceneRect(), Qt::KeepAspectRatio);
-    ui->graphicsView->show();
+		q_svg_renderer = std::make_unique<QSvgRenderer>(&xml); // is this ok? when this function runs out of scope, xml is deleted, but does q_svg_renderer still use the xml?
+		//q_svg_renderer = std::unique_ptr<QSvgRenderer>(new QSvgRenderer(&xml));
 
-    //auto foo = ui->centralwidget->children().size();
-    //this->ui->graphicsView.
+		/*
+		q_graphics_svg_item = std::make_unique<QGraphicsSvgItem>(); // depends on q_svg_renderer -> destroy order
+		q_graphics_svg_item->setSharedRenderer(q_svg_renderer.get()); // does not take ownership
+		*/
+		auto local_q_graphics_svg_item = new QGraphicsSvgItem(); // depends on q_svg_renderer -> destroy order
+		local_q_graphics_svg_item->setSharedRenderer(q_svg_renderer.get()); // does not take ownership
+
+
+		q_graphics_scene = std::make_unique<QGraphicsScene>();
+		q_graphics_scene->addItem(local_q_graphics_svg_item); // takes ownership
+
+		
+		// todo: whenever setting a new Scene, do not reuse the exactly same unique_ptrs..
+		// first build up the new bunch of objects,
+		// then setScene
+		// then destroy old objects in reverse order compared to construction...
+
+
+		ui->graphicsView->setScene(q_graphics_scene.get());
+		ui->graphicsView->fitInView(q_graphics_scene.get()->sceneRect(), Qt::KeepAspectRatio);
+		ui->graphicsView->show();
+
+		/*QSvgRenderer* svgRenderer = new QSvgRenderer(&xml);
+		QGraphicsSvgItem *item = new QGraphicsSvgItem();
+		QGraphicsScene *scene = new QGraphicsScene();
+
+		item->setSharedRenderer(svgRenderer);
+		scene->addItem(item);
+		ui->graphicsView->setScene(scene);
+		ui->graphicsView->fitInView(scene->sceneRect(), Qt::KeepAspectRatio);
+		ui->graphicsView->show();*/
+
+		//auto foo = ui->centralwidget->children().size();
+		//this->ui->graphicsView.
+	}
 }
 
 
 void MainWindow::on_actionAbout_triggered()
 {
-    QMessageBox msgBox;
-    msgBox.setText(QString("Qt Version used:   ") + qVersion());
-    msgBox.exec();
+	QMessageBox msgBox;
+	msgBox.setText(QString("Qt Version used:   ") + qVersion());
+	msgBox.exec();
 }
 
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -7,6 +7,8 @@
 #include <QSvgRenderer>
  #include <QGraphicsSvgItem>
 
+#include <memory>
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -27,6 +29,11 @@ private slots:
 
 private:
     Ui::MainWindow *ui;
+
+    std::unique_ptr<QSvgRenderer> q_svg_renderer;
+    std::unique_ptr<QGraphicsSvgItem> q_graphics_svg_item;
+    std::unique_ptr<QGraphicsScene> q_graphics_scene;
+
 };
 
 #endif // MAINWINDOW_H
