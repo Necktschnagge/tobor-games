512131747072913e5c5bf42628e3073fc7fc7402
Maximilian Starke <maximilian.starke@medionmail.com>
2024-02-16 22:52:42 +0100

    minor

--- src/key_event_filter.cpp
+++ src/key_event_filter.cpp
@@ -1,1 +1,19 @@
 #include "key_event_filter.h"
+
+#include<QKeyEvent>
+
+bool ControlKeyEventAgent::eventFilter(QObject* object, QEvent* e)
+{
+	QKeyEvent* keyEvent = dynamic_cast<QKeyEvent*>(e);
+
+	if (keyEvent == nullptr)
+		return false; //pass-through
+
+	// found a keyEvent
+
+	//switch (keyEvent->key()) {
+	//	
+	//}
+
+	return true; //absorbing event
+}
