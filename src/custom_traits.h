3cfe3b9510f7c1faa32c6e9bf7694f0d206f3450
Maximilian Starke <maximilian.starke@medionmail.com>
2024-02-16 16:18:32 +0100

    try another implementation

--- src/custom_traits.h
+++ src/custom_traits.h
@@ -3,6 +3,7 @@
 #include <QSignalMapper>
 
 #include <utility>
+#include <type_traits>
 
 namespace _Internal_Implementation_Tobor_Traits
 {
@@ -41,14 +42,54 @@ namespace _Internal_Implementation_Tobor_Traits
 	*/
 
 	template <class T, template <typename TU> class Function_User, class Dummy>
-	struct select_member_function : public std::false_type {
+	struct select_member_function {
+
+		static constexpr bool value{ false };
+
 		//static const auto mappetIntPtr{ get_mapped_ptr<QSignalMapper, void>::mappetPtr };
 	};
 
 	template <class T, template <typename TU> class Function_User>
-	struct select_member_function<T, Function_User, void> : public std::true_type {
+	struct select_member_function<T, Function_User, void> {
+
+		static constexpr bool value{ true };
+
 		using user = Function_User<T>;
 		//inline static const auto mappetIntPtr{ get_mapped_int_ptr<QSignalMapper>::mappetIntPtr };
+	};
+
+// Primary template with a static assertion
+// for a meaningful error message
+// if it ever gets instantiated.
+// We could leave it undefined if we didn't care.
+
+	template<typename, typename T>
+	struct has_m {
+		static_assert(
+			std::integral_constant<T, false>::value,
+			"Second template parameter needs to be of function type.");
+	};
+
+	// specialization that does the checking
+
+	template<typename C, typename Ret, typename... Args>
+	struct has_m<C, Ret(Args...)> {
+	private:
+		template<typename T>
+		static constexpr auto check(T*)
+			-> typename
+			std::is_same<
+			decltype(std::declval<T>().mappedInt(std::declval<Args>()...)),
+			Ret    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+			>::type;  // attempt to call it and see if the return type is correct
+
+		template<typename>
+		static constexpr std::false_type check(...);
+
+		typedef decltype(check<C>(0)) type;
+
+	public:
+		static constexpr bool value = type::value;
 	};
 
 	
@@ -73,6 +114,10 @@ static_assert(
 	void>
 	::value,
 	"test member function prescence"); // does not work
+
+static_assert(
+	_Internal_Implementation_Tobor_Traits::has_m<QSignalMapper, void(int)>::value,
+	"2nd try to check"); // does not work
 
 namespace {
 	struct example {
