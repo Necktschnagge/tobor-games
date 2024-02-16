e709ce22bc6b725799274ae4229a7fe61ba9feed
Maximilian Starke <maximilian.starke@medionmail.com>
2024-02-16 16:28:24 +0100

    test for negation in static assert

--- src/custom_traits.h
+++ src/custom_traits.h
@@ -7,14 +7,6 @@
 
 namespace _Internal_Implementation_Tobor_Traits
 {
-	template <class T>
-	struct member_function_user {
-		static void test(T& t) {
-			t.mappedInt(5);
-
-		}
-		
-	};
 
 	template <class T, bool HAS_mappedInt>
 	struct get_mapped_ptr;
@@ -27,35 +19,6 @@ namespace _Internal_Implementation_Tobor_Traits
 	template <class T>
 	struct get_mapped_ptr<T, true> {
 		inline static const auto mappetPtr{ &T::mappedInt };
-	};
-
-	/*
-	template <class T>
-	struct get_mapped_int_ptr {
-		inline static const auto mappetIntPtr{ &T::mappedInt };
-	};
-	
-	template <>
-	struct get_mapped_int_ptr<QSignalMapper> {
-		inline static const auto mappetIntPtr{ nullptr };
-	};
-	*/
-
-	template <class T, template <typename TU> class Function_User, class Dummy>
-	struct select_member_function {
-
-		static constexpr bool value{ false };
-
-		//static const auto mappetIntPtr{ get_mapped_ptr<QSignalMapper, void>::mappetPtr };
-	};
-
-	template <class T, template <typename TU> class Function_User>
-	struct select_member_function<T, Function_User, void> {
-
-		static constexpr bool value{ true };
-
-		using user = Function_User<T>;
-		//inline static const auto mappetIntPtr{ get_mapped_int_ptr<QSignalMapper>::mappetIntPtr };
 	};
 
 // Primary template with a static assertion
@@ -95,13 +58,6 @@ namespace _Internal_Implementation_Tobor_Traits
 	
 } // namespace detail
 
-//static constexpr bool QSignalMapper_HAS_mappedInt{
-//	   _Internal_Implementation_Tobor_Traits::select_member_function<
-//   QSignalMapper,
-//   _Internal_Implementation_Tobor_Traits::member_function_user,
-//   void>
-//   ::value
-//};
 static constexpr bool QSignalMapper_HAS_mappedInt{
 	_Internal_Implementation_Tobor_Traits::has_m<QSignalMapper, void(int)>::value
 };
@@ -111,15 +67,7 @@ inline static auto mapped_ptr{
 };
 
 static_assert(
-	_Internal_Implementation_Tobor_Traits::select_member_function<
-	QSignalMapper,
-	_Internal_Implementation_Tobor_Traits::member_function_user,
-	void>
-	::value,
-	"test member function prescence"); // does not work
-
-static_assert(
-	_Internal_Implementation_Tobor_Traits::has_m<QSignalMapper, void(int)>::value,
+	!_Internal_Implementation_Tobor_Traits::has_m<QSignalMapper, void(int)>::value,
 	"2nd try to check"); // does not work
 
 namespace {
