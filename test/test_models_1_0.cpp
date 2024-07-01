062ac8bb37ecadf132e1ddd0f1fea7d479c2c456
Maximilian Starke <maximilian.starke@medionmail.com>
2024-07-01 11:39:33 +0200

    Fix Test only failing to compile on CI WIN 2019 (#161)
    * I
    
    * II
    
    * III
    
    * IIII
    
    * Revert "IIII"
    
    This reverts commit 17047db7e29ed22a1559152a0752df9b17323a97.
    
    * X-1
    
    * X-2
    
    * ci test compile
    
    * tests, finalize for models 1 0

--- test/test_models_1_0.cpp
+++ test/test_models_1_0.cpp
@@ -27,7 +27,6 @@ TEST(tobor__v1_0__direction, default_ctor) {
 
 	ASSERT_TRUE(true);
 }
-
 TEST(tobor__v1_0__direction, invert) {
 
 	EXPECT_EQ((!tobor::v1_0::direction::NORTH()), tobor::v1_0::direction::SOUTH());
@@ -125,10 +124,10 @@ TEST(tobor__v1_0__piece_id, create) {
 
 TEST(tobor__v1_0__piece_move, create_and_compare) {
 
-	auto x = tobor::v1_0::piece_move(tobor::v1_0::piece_id(7), tobor::v1_0::direction::SOUTH());
-	auto y = tobor::v1_0::piece_move(tobor::v1_0::piece_id(8), tobor::v1_0::direction::NORTH());
+	auto x = tobor::v1_0::piece_move(tobor::v1_0::default_piece_id(7), tobor::v1_0::direction::SOUTH());
+	auto y = tobor::v1_0::piece_move(tobor::v1_0::default_piece_id(8), tobor::v1_0::direction::NORTH());
 
-	EXPECT_EQ(x.pid, 7);
+	EXPECT_EQ(x.pid, tobor::v1_0::default_piece_id::int_type(7));
 	EXPECT_EQ(y.dir, tobor::v1_0::direction::NORTH());
 
 	EXPECT_TRUE(x < y);
@@ -142,86 +141,3 @@ TEST(tobor__v1_0__move_path, create) {
 	);
 
 }
-
-
-
-
-
-
-TEST(engine, example_integration) {
-	// check the following scenario:
-	// create world
-	// create initial robots position
-	// run solver
-	/*
-	auto w = tobor::v1_0::default_world(16, 16);
-
-	w.block_center_cells(2, 2);
-
-	// add vertical walls row by row, starting left
-	w.west_wall_by_id(w.coordinates_to_cell_id(6, 0)) = true;
-	w.west_wall_by_id(w.coordinates_to_cell_id(12, 0)) = true;
-	w.west_wall_by_id(w.coordinates_to_cell_id(2, 1)) = true;
-	w.west_wall_by_id(w.coordinates_to_cell_id(10, 1)) = true;
-	w.west_wall_by_id(w.coordinates_to_cell_id(14, 3)) = true;
-	w.west_wall_by_id(w.coordinates_to_cell_id(1, 4)) = true;
-	w.west_wall_by_id(w.coordinates_to_cell_id(11, 4)) = true;
-	w.west_wall_by_id(w.coordinates_to_cell_id(13, 5)) = true;
-	w.west_wall_by_id(w.coordinates_to_cell_id(4, 6)) = true;
-	w.west_wall_by_id(w.coordinates_to_cell_id(12, 9)) = true;
-	w.west_wall_by_id(w.coordinates_to_cell_id(7, 10)) = true;
-	w.west_wall_by_id(w.coordinates_to_cell_id(14, 10)) = true;
-	w.west_wall_by_id(w.coordinates_to_cell_id(3, 11)) = true;
-	w.west_wall_by_id(w.coordinates_to_cell_id(7, 13)) = true;
-	w.west_wall_by_id(w.coordinates_to_cell_id(10, 13)) = true;
-	w.west_wall_by_id(w.coordinates_to_cell_id(1, 14)) = true;
-	w.west_wall_by_id(w.coordinates_to_cell_id(13, 14)) = true;
-	w.west_wall_by_id(w.coordinates_to_cell_id(4, 15)) = true;
-	w.west_wall_by_id(w.coordinates_to_cell_id(12, 15)) = true;
-
-	w.south_wall_by_transposed_id(w.coordinates_to_transposed_cell_id(0, 2)) = true;
-	w.south_wall_by_transposed_id(w.coordinates_to_transposed_cell_id(0, 10)) = true;
-	w.south_wall_by_transposed_id(w.coordinates_to_transposed_cell_id(1, 4)) = true;
-	w.south_wall_by_transposed_id(w.coordinates_to_transposed_cell_id(1, 14)) = true;
-	w.south_wall_by_transposed_id(w.coordinates_to_transposed_cell_id(2, 2)) = true;
-	w.south_wall_by_transposed_id(w.coordinates_to_transposed_cell_id(2, 11)) = true;
-	w.south_wall_by_transposed_id(w.coordinates_to_transposed_cell_id(3, 7)) = true;
-	w.south_wall_by_transposed_id(w.coordinates_to_transposed_cell_id(6, 3)) = true;
-	w.south_wall_by_transposed_id(w.coordinates_to_transposed_cell_id(6, 14)) = true;
-	w.south_wall_by_transposed_id(w.coordinates_to_transposed_cell_id(7, 11)) = true;
-	w.south_wall_by_transposed_id(w.coordinates_to_transposed_cell_id(9, 2)) = true;
-	w.south_wall_by_transposed_id(w.coordinates_to_transposed_cell_id(9, 13)) = true;
-	w.south_wall_by_transposed_id(w.coordinates_to_transposed_cell_id(10, 4)) = true;
-	w.south_wall_by_transposed_id(w.coordinates_to_transposed_cell_id(11, 10)) = true;
-	w.south_wall_by_transposed_id(w.coordinates_to_transposed_cell_id(13, 6)) = true;
-	w.south_wall_by_transposed_id(w.coordinates_to_transposed_cell_id(13, 15)) = true;
-	w.south_wall_by_transposed_id(w.coordinates_to_transposed_cell_id(14, 3)) = true;
-	w.south_wall_by_transposed_id(w.coordinates_to_transposed_cell_id(14, 10)) = true;
-	w.south_wall_by_transposed_id(w.coordinates_to_transposed_cell_id(15, 7)) = true;
-	w.south_wall_by_transposed_id(w.coordinates_to_transposed_cell_id(15, 12)) = true;
-
-	// specify initial state
-	auto target_piece = tobor::v1_0::default_cell_id::create_by_coordinates(6, 9, w);
-	auto green_robot = tobor::v1_0::default_cell_id::create_by_coordinates(12, 7, w);
-	auto red_robot = tobor::v1_0::default_cell_id::create_by_coordinates(12, 12, w);
-	auto yellow_robot = tobor::v1_0::default_cell_id::create_by_coordinates(6, 14, w);
-
-	std::array<tobor::v1_0::default_cell_id, 3> other_robots{ green_robot, red_robot, yellow_robot };
-	std::array<tobor::v1_0::default_cell_id, 1> target_robots{ target_piece };
-
-	//auto initial_state = tobor::v1_0::positions_of_pieces<3>(target_piece, std::move(other_robots));
-
-	// specify target field
-	auto target = tobor::v1_0::default_cell_id::create_by_coordinates(9, 1, w);
-
-	auto w_analyzer = tobor::v1_0::default_move_one_piece_calculator(w);
-
-	auto partial_state_graph = tobor::v1_0::partial_state_graph<tobor::v1_0::default_move_one_piece_calculator>(target_robots, other_robots);
-
-	partial_state_graph.explore_until_optimal_solution_distance(w_analyzer, target);
-
-	ASSERT_EQ(9, partial_state_graph.get_optimal_path_length());
-	*/
-
-}
-
