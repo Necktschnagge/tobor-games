426c8039e056810ce145d74a99c659cc0324eb08
Maximilian Starke <maximilian.starke@medionmail.com>
2024-10-06 11:45:54 +0200

    remove debug code from main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -21,39 +21,10 @@ PIECE                   :       one robot / piece
 
 */
 
-class Dummy {
-
-	uint32_t data;
-
-public:
-	Dummy(const uint32_t& data) : data(data % (((uint32_t)1) << 24)){
-
-	}
-
-	static constexpr std::size_t byte_size() { return 3; }
-
-	inline uint8_t get_byte(std::size_t index) const {
-		return (data >> (index * 8)) & 0xFF;
-	}
-};
-
-void experimental() {
-	fsl::byte_tree_map<Dummy, uint16_t> btm(1023);
-
-	Dummy d1(16), d2(974), d3(16 + (7 << 16)), d4(1024);
-
-	btm[d1] = 1;
-	btm[d2] = 2;
-	btm[d3] = 3;
-	btm[d4] = 4;
-
-}
 
 int main(int argc, char* argv[])
 {
 	init_logger();
-
-	experimental();
 
 	QApplication qt_app(argc, argv);
 	std::setlocale(LC_NUMERIC, "C");
