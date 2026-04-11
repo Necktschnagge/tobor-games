64fca51e8004b0eae445e5c09d56a846f5b8c0aa
Maximilian Starke <maximilian.starke@medionmail.com>
2026-04-11 10:25:23 +0200

    need to rename a file since this name is reserved for c libraries and has file naming conflicts

---
+++ src/tobor_math.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cstdint>
+
+namespace tobor {
+
+	template<std::uint64_t X>
+	inline constexpr std::uint64_t FACULTY{ X * FACULTY<X - 1> };
+
+	template<>
+	inline constexpr std::uint64_t FACULTY<0>{ 1 };
+}
